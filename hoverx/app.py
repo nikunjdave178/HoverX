0403e893deba198f62388695dd441ef28771f385
Nikunj <nikunjd@meditab.com>
2025-12-20 11:58:35 +0530

    Add robust auto-updater (check, download, replace, restart)

--- hoverx/app.py
+++ hoverx/app.py
@@ -1,10 +1,12 @@
-import sys,os
+import sys,os,subprocess
 from PyQt6.QtWidgets import QApplication, QSystemTrayIcon, QMenu
 from PyQt6.QtGui import QIcon
 from PyQt6.QtCore import Qt
 from hoverx.ui.widget import FloatingWidget
 from hoverx.controller.dummy import DummyController
 from hoverx.version import __version__
+from hoverx.updater.checker import check_for_update
+from hoverx.updater.downloader import download_update
 
 try:
     from hoverx.controller.windows_smtc import WindowsSMTCController
@@ -13,44 +15,64 @@ except Exception as e:
     controller = DummyController()
 
 def main():
-    print("HoverX version:", __version__)
+    update_info = check_for_update()
+
+    if update_info:
+        try:
+            new_path = download_update(update_info["url"])
+            trigger_update(new_path)
+            return
+        except Exception:
+            pass
 
     app = QApplication(sys.argv)
-
     app.setApplicationName("HoverX")
     app.setOrganizationName("HoverX")
+
+    try:
+        from hoverx.controller.windows_smtc import WindowsSMTCController
+        controller = WindowsSMTCController()
+    except Exception:
+        controller = DummyController()
 
     widget = FloatingWidget(controller)
     widget.show()
 
-    # ----- System Tray -----
-    tray = QSystemTrayIcon(widget)
-    icon = QIcon(resource_path("assets/icons/icon.ico"))
-    tray.setIcon(icon)  # MUST NOT be empty
+    tray = QSystemTrayIcon()
+    tray.setParent(app)
+    tray.setIcon(QIcon(resource_path("assets/icons/icon.ico")))
     tray.setToolTip("HoverX")
 
     menu = QMenu()
-
-    show_action = menu.addAction("Show HoverX")
-    hide_action = menu.addAction("Hide HoverX")
+    menu.addAction("Show HoverX", widget.show)
+    menu.addAction("Hide HoverX", widget.hide)
     menu.addSeparator()
-    exit_action = menu.addAction("Exit HoverX")
-
-    show_action.triggered.connect(widget.show)
-    hide_action.triggered.connect(widget.hide)
-    exit_action.triggered.connect(app.quit)
+    menu.addAction("Exit HoverX", app.quit)
 
     tray.setContextMenu(menu)
     tray.show()
 
     app.setQuitOnLastWindowClosed(False)
-
     sys.exit(app.exec())
+
 
 def resource_path(relative_path):
     if getattr(sys, "frozen", False):
         return os.path.join(sys._MEIPASS, relative_path)
     return relative_path
+
+def trigger_update(new_exe_path):
+    current_exe = sys.executable
+
+    updater = r"E:\Projects\HoverX\dist"  # TEMP, hardcoded for now
+
+    subprocess.Popen([
+        updater,
+        current_exe,
+        new_exe_path
+    ])
+
+    sys.exit(0)
 
 if __name__ == "__main__":
     main()
---
+++ hoverx/updater/checker.py
@@ -0,0 +1,24 @@
+import requests
+from packaging import version
+from hoverx.version import __version__
+
+UPDATE_URL = "https://raw.githubusercontent.com/nikunjdave178/HoverX/main/update.json"
+
+def check_for_update(timeout=3):
+    try:
+        resp = requests.get(UPDATE_URL, timeout=timeout)
+        resp.raise_for_status()
+        data = resp.json()
+
+        remote_version = version.parse(data.get("version", "0.0.0"))
+        local_version = version.parse(__version__)
+
+        if remote_version > local_version:
+            return data
+
+    except Exception as ex:
+        if not getattr(__import__("sys"), "frozen", False):
+            print("Update check failed:", ex)
+        return None
+
+    return None
